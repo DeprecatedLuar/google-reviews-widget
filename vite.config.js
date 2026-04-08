fdc5de240087452d415966d10ef8ba14db3358c4
DeprecatedLuar <rauassis2@gmail.com>
2026-04-08 08:25:30 -0300

    update structure

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  root: "dev",
+  publicDir: "public",
   build: {
     lib: {
-      entry: "src/widget-entry.jsx",
+      entry: resolve(__dirname, "src/widget-entry.jsx"),
       name: "ReviewsWidget",
       formats: ["iife"],
       fileName: () => "reviews-widget.js",
     },
+    outDir: resolve(__dirname, "dist"),
+    emptyOutDir: true,
   },
 });
